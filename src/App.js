8d6a460dfcdaf70efd6aa64ed33ef49ffa984875
alexAkshay <93480740+alexAkshay@users.noreply.github.com>
2022-06-29 21:19:19 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import './App.css';
-import Weather from './Components/Weather';
-import Time from './Components/Time';
+
+import React, {useState, useEffect } from 'react';
+import Main from './Main';
+import ThemesContext, {themes} from './Themes';
 
 function App() {
-  return (
-    <div className="App">
-      <Time/>
-      <Weather/>
+  const [theme, setTheme] = useState(themes.nightTime)
+  
+  let currentHour = new Date().getHours();
+  // let currentHour = 23
+  
+  
+  useEffect(()=>{
+   
 
-    </div>
+    if(0 <= currentHour && currentHour <= 6){
+      setTheme(themes.nightTime)
+    }else{
+      if(13<= currentHour && currentHour <= 17){
+        setTheme(themes.dayTime)
+      }else{
+        if(18 <= currentHour && currentHour<= 21){
+          setTheme(themes.eveningTime)
+        }else{
+          if(7<= currentHour && currentHour <= 12){
+            setTheme(themes.morningTime)
+          }else{
+            setTheme(themes.nightTime)
+          }
+        }
+      }
+    }
+
+    
+    
+    
+   
+
+   
+  }, [currentHour])
+
+  return (
+    <ThemesContext.Provider value={theme}>
+      <Main/>
+    </ThemesContext.Provider>
   );
 }
 
